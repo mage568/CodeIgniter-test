9313f5dcd9400993fcb21d871ca0b27345a812f9
DESKTOP-MIS84OG\admin <mage568@hotmail.com>
2018-12-24 06:56:09 -0800

    add test-controller

---
+++ application/controllers/Test.php
@@ -0,0 +1,10 @@
+<?php
+    class Test extends CI_Controller {
+        public function index() {
+            echo "This is default function";
+        }
+        public function hello() {
+            echo "This is Hello function";
+        }
+    }
+?>
