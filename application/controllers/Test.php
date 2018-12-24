757b8bc0ae004d9fb842d96f99d5e9be317487e9
DESKTOP-MIS84OG\admin <mage568@hotmail.com>
2018-12-24 07:30:43 -0800

    add test-view file

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,7 +1,7 @@
 <?php
     class Test extends CI_Controller {
         public function index() {
-            echo "This is default function";
+            $this->load->view('test');
         }
         public function hello() {
             echo "This is Hello function";
